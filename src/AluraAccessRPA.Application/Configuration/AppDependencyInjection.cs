1fdd27a4fa1094ac46ee80e6f87587c9a1776e1f
Sabryna Alves <sabryna.alves@interfile.com.br>
2024-01-16 21:21:25 -0300

    Construindo estrutura

---
+++ src/AluraAccessRPA.Application/Configuration/AppDependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraAccessRPA.Application.Configuration
+{
+    public class AppDependencyInjection
+    {
+        public static IServiceCollection AddAppDependencies(this IServiceCollection services)
+        {
+            //! Alterar para repositorio verdadeiro
+            services.AddTransient<IApiExcelExpert, ApiExcelexpert>();
+            services.AddSingleton<IFileService, FileService>();
+
+            return services;
+        }
+    }
+}
--- src/AluraAccessRPA.Worker/Program.cs
+++ src/AluraAccessRPA.Worker/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Microsoft.Extensions.Hosting;
+
+IConfiguration Configuration = default;
+IHostEnvironment HostingEnvironment = default;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        HostingEnvironment = context.HostingEnvironment;
+        Configuration = new ConfigurationBuilder()
+            .SetBasePath(context.HostingEnvironment.ContentRootPath)
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+        services.AddAppDependencies();
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddQuartz(q =>
+        {
+            q.UseMicrosoftDependencyInjectionJobFactory();
+            q.AddJobAndTrigger<WebRequestJob>(Configuration);
+
+        })
+        .AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+        services.AddLogging(logger =>
+        {
+            logger.ClearProviders();
+            logger.SetMinimumLevel(LogLevel.Information);
+            logger.AddNLog("NLog.config");
+        });
+    })
+    .UseWindowsService()
+    .Build();
+
+await host.RunAsync();
